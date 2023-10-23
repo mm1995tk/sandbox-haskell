bd91bb295409e7134199219c7ead6a9c4425c6b9
mm1995tk <jimi.woodstock1995@gmail.com>
2023-10-24 00:16:04 +0900

    環境変数から設定値を取るように修正

--- src/MyLib/Support.hs
+++ src/MyLib/Support.hs
@@ -1,11 +1,27 @@
 module MyLib.Support where
 
+import Data.Maybe (fromMaybe)
 import Data.Time (secondsToDiffTime)
+import GHC.Word (Word16)
 import Hasql.Connection (settings)
 import Hasql.Pool
+import System.Environment (getEnv)
+import System.Posix.Env.ByteString qualified as PEB
 
 getPool :: IO Pool
-getPool = acquire 10 (secondsToDiffTime 30) (secondsToDiffTime 30 * 5) $ settings "localhost" 5431 "postgres" "postgres" "postgres"
+getPool = do
+  host <- getEnv' "HOST"
+  port <- read @Word16 <$> getEnv "POSTGRES_PORT"
+  user <- getEnv' "PGUSER"
+  password <- getEnv' "PGPASSWORD"
+  database <- getEnv' "PGDATABASE"
+  poolSize <- read @Int <$> getEnv "CONNECTION_POOL_SIZE"
+  lifetime <- read @Integer <$> getEnv "MAXIMAL_CONNECTION_LIFETIME"
+  idletime <- read @Integer <$> getEnv "MAXIMAL_CONNECTION_IDLE_TIME"
+
+  acquire poolSize (secondsToDiffTime lifetime) (secondsToDiffTime idletime) $ settings host port user password database
+ where
+  getEnv' s = fromMaybe (error "does not exist (no environment variable)") <$> PEB.getEnv s
 
 release' :: Pool -> IO ()
 release' c = putStrLn "released" *> release c
