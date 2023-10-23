8ac2781d09449844699e5c3e29f64fc0280b62a9
mm1995tk <jimi.woodstock1995@gmail.com>
2023-10-23 23:14:50 +0900

    不要なコード削除

--- src/MyLib/Support.hs
+++ src/MyLib/Support.hs
@@ -1,33 +1,11 @@
 module MyLib.Support where
 
-import Control.Concurrent.Async (concurrently)
 import Data.Time (secondsToDiffTime)
 import Hasql.Connection (settings)
 import Hasql.Pool
-import Hasql.Session
-import Hasql.Transaction qualified as Tx
-import Hasql.Transaction.Sessions
-
--- inConnection :: (Pool -> IO a) -> IO a
--- inConnection f = getPool >>= \pool -> f pool <* release' pool
-defaultTx :: Tx.Transaction a -> Session a
-defaultTx = transaction RepeatableRead Write
-
-inConnection :: IO (Session a) -> IO (Either UsageError a)
-inConnection f = do
-  (pool, f') <- concurrently getPool f
-  use pool f' <* release' pool
-
-notRelease :: IO (Session a) -> IO (Pool, Either UsageError a)
-notRelease f = do
-  (pool, f') <- concurrently getPool f
-  (pool,) <$> use pool f'
 
 getPool :: IO Pool
 getPool = acquire 10 (secondsToDiffTime 30) (secondsToDiffTime 30 * 5) $ settings "localhost" 5431 "postgres" "postgres" "postgres"
-
--- cleanUpDb :: IO (Either UsageError ())
--- cleanUpDb = inConnection $ statement () $ Statement "truncate person" E.noParams D.noResult True
 
 release' :: Pool -> IO ()
 release' c = putStrLn "released" *> release c
