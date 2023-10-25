35bf6ec64272a0add57753390c885f4a29f1b5c2
mm1995tk <jimi.woodstock1995@gmail.com>
2023-10-26 01:22:00 +0900

    クエリパラメータをSumTypesにマッピング

--- src/ApiExample/Endpoint/ListUsers.hs
+++ src/ApiExample/Endpoint/ListUsers.hs
@@ -2,18 +2,38 @@ module ApiExample.Endpoint.ListUsers where
 
 import ApiExample.Domain (Person)
 import ApiExample.Framework (AppCtx (..), ServerM)
-import Control.Monad.Reader (MonadReader (..), ask)
 import ApiExample.Infrastructure (findAll)
+import Control.Monad.Reader (MonadReader (..), ask)
+import Control.Monad.Trans (liftIO)
 import Data.Text (Text)
 import Data.Vector qualified as Vec
-import Servant (Get, Header, JSON, (:>))
+import Servant (FromHttpApiData (parseQueryParam), Get, Header, JSON, QueryParam, (:>))
+
+data OrderBy = Asc | Desc
+
+instance FromHttpApiData OrderBy where
+  parseQueryParam "asc" = Right Asc
+  parseQueryParam "desc" = Right Desc
+  parseQueryParam _ = Left "must be \"asc\" or \"desc\""
 
 type ListUser =
   "users"
     :> Header "user-agent" Text
+    :> QueryParam "order-by" OrderBy
     :> Get '[JSON] (Vec.Vector Person)
 
 handleGetUsers :: ServerM ListUser
-handleGetUsers _ = do
+handleGetUsers _ Nothing = do
+  liftIO $ print "none"
+  AppCtx{runDBIO} <- ask
+  runDBIO findAll ()
+handleGetUsers _ (Just Asc) = do
+  liftIO $ print "asc"
+
+  AppCtx{runDBIO} <- ask
+  runDBIO findAll ()
+handleGetUsers _ (Just Desc) = do
+  liftIO $ print "desc"
+
   AppCtx{runDBIO} <- ask
   runDBIO findAll ()
