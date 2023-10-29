77a594b0ed124c317bfcd51397422188d517da5b
mm1995tk <jimi.woodstock1995@gmail.com>
2023-10-29 19:23:23 +0900

    ulidを扱うUtilsの追加

--- src/MyLib/Utils.hs
+++ src/MyLib/Utils.hs
@@ -3,6 +3,10 @@
 module MyLib.Utils where
 
 import Control.Concurrent (threadDelay)
+import Data.Either (fromRight)
+import Data.Text qualified as T
+import Data.ULID (ULID, ulidFromInteger)
+import Data.ULID.Base32 (decode)
 
 threadDelaySec :: Int -> IO ()
 threadDelaySec = threadDelay . (* 1000000)
@@ -12,3 +16,11 @@ arg |> fn = fn arg
 
 pipeline :: a -> (a -> b) -> b
 pipeline = (|>)
+
+ulidFromText :: T.Text -> ULID
+ulidFromText t = case decode 26 t of
+  [(i, _)] -> fromRight (error "") $ ulidFromInteger i
+  _ -> error ""
+
+ulidToText :: ULID -> T.Text
+ulidToText = T.pack . show
