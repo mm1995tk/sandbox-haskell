cbc1b43ee0205db039a337497ca83bba1abb8052
mm1995tk <jimi.woodstock1995@gmail.com>
2023-11-13 20:41:55 +0900

    code firstに変更

--- src/ApiExample/GraphQL/API.hs
+++ src/ApiExample/GraphQL/API.hs
@@ -1,26 +1,59 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+
 module ApiExample.GraphQL.API (gqlApi) where
 
-import Data.Morpheus (deriveApp, App)
-import Data.Morpheus.Document (importGQLDocument)
-import Data.Morpheus.Types (RootResolver (..), Undefined)
-
+import Data.Morpheus (App, deriveApp)
+import Data.Morpheus.Types (GQLType, ResolverQ, RootResolver (..), Undefined)
 import Data.Text (Text)
+import GHC.Generics (Generic)
 
-importGQLDocument "src/ApiExample/schema.gql"
+data Query m = Query
+  { deity :: DeityArgs -> m Deity
+  , deityAll :: m [Deity]
+  }
+  deriving (Generic, GQLType)
+
+data Deity = Deity
+  { name :: Text
+  , power :: Maybe Text
+  }
+  deriving (Generic, GQLType)
+
+data DeityArgs = DeityArgs
+  { name :: Text
+  , pow :: Maybe Text
+  }
+  deriving (Generic, GQLType)
 
 rootResolver :: RootResolver IO () Query Undefined Undefined
 rootResolver =
   RootResolver
-    { queryResolver = Query{deity}
+    { queryResolver = Query{deity, deityAll}
     , mutationResolver = undefined
     , subscriptionResolver = undefined
     }
  where
-  deity DeityArgs{name, pow} = do
+  deityAll :: ResolverQ e IO [Deity]
+  deityAll =
+    return
+      [ Deity
+          { name = "name01"
+          , power = Just "pow1"
+          }
+      , Deity
+          { name = "name02"
+          , power = Just "pow2"
+          }
+      ]
+  deity :: DeityArgs -> ResolverQ e IO Deity
+  deity DeityArgs{name, pow} =
     pure
       Deity
-        { name = pure name
-        , power = pure (Just pow)
+        { name = name
+        , power = pow
         }
 
 gqlApi :: App () IO
