6835f3143c4803e48a4d988b96d083a41459e974
mm1995tk <jimi.woodstock1995@gmail.com>
2023-11-22 20:14:44 +0900

    cookie追加

--- tmp/index.ts
+++ tmp/index.ts
@@ -5,14 +5,18 @@ const url = "http://127.0.0.1:8080";
 
 const openapiClient = createClient<paths>({ baseUrl: url });
 
+const headers = new Headers();
+headers.set("cookie", "session-id=xxx");
+
 const r = await openapiClient.GET("/users/{user-id}", {
   params: {
-    path: { "user-id": "01HE8J6FXQNK3V36RNZGVH95MX" },
+    path: { "user-id": "01HE8J6FXQNK3V36RNZGVH95MX!" },
   },
+  headers,
 });
 
 if (r.data) {
   console.log(r.data);
 } else {
-  console.log(r.error);
+  console.log(r.response.status);
 }
